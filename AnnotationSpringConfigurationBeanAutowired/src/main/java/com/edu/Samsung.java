8f0d482c6f2293d6a6de3ce563ed0333dc44dbc1
GOMATHI KAVI <GOMATHI KAVI@DESKTOP-AB6DJ25>
2022-04-28 20:39:36 -0700

    GitHub

---
+++ AnnotationSpringConfigurationBeanAutowired/src/main/java/com/edu/Samsung.java
@@ -0,0 +1,29 @@
+package com.edu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Samsung {
+	@Autowired
+	MobileProcessor mcpu;
+
+	
+	public MobileProcessor getMcpu() {
+		return mcpu;
+	}
+
+
+
+	public void setMcpu(MobileProcessor mcpu) {
+		this.mcpu = mcpu;
+	}
+
+
+
+	public void MobileConfig() {
+		System.out.println("8gb Ram Dual Core 128px camera");
+		mcpu.processor();
+	}
+
+}
+
+
---
+++ AnnotationSpringConfigurationBeanAutowired/src/main/java/com/edu/SnapDragon.java
@@ -0,0 +1,12 @@
+package com.edu;
+
+public class SnapDragon implements MobileProcessor {
+
+	public void processor() {
+		System.out.println("World Best Processor");
+		
+	}
+
+}
+
+
